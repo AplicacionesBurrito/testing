728dc873f54e543065679febcada861be39467b7
marcelcelaya <marcel.celaya@uabc.edu.mx>
2017-09-14 15:42:30 -0700

    Se agregor la funcion de generar y de imprimir los numeros desordenados

--- OrdenarNumeros.c
+++ OrdenarNumeros.c
@@ -2,15 +2,45 @@
 #include <stdio.h>
 #include <time.h>
 void Ordenar(int vectorDesordenado[10000],int vectorOrdenado[10000]);
+void Generar(int vectorDesordenado[10000]);
+void imprimir(int vectorDesordenado[10000]);
 int main(){
-	int vectorDesordenado[10000],vectorOrdenado[10000];
+	int op, vectorDesordenado[10000],vectorOrdenado[10000];
 	do{
+		system("cls");
 		printf("1) Genera los 10000 numeros aleatorios\n");
 		printf("2) Imprime los 10000 numeros aleatorios\n");
 		printf("3) Ordena los 10000 numeros\n");
 		printf("4) Imprime los 10000 numeros ya ordenados\n");
 		printf("Presiona ESC para salir\n");
+		scanf("%d",&op);
+		switch(op){
+			case 1: Generar(vectorDesordenado); break;
+			case 2: imprimir(vectorDesordenado); break;
+		}
 	}while(op!=27);
 	system ("pause");
 	return 0;
 }
+
+void Generar(int vectorDesordenado[10000])
+{
+	int i;
+	system("cls");
+	for(i=0;i<10000;i++){
+		vectorDesordenado[i]=rand()%10000;
+	}
+	printf("Vector generado\n");
+	system("pause");
+}
+void imprimir(int vectorDesordenado[10000])
+{
+	int i;
+	system("cls");
+	for( i=0;i<10000;i++){
+		printf("%d\n",vectorDesordenado[i]);
+	}
+	printf("Vector Impreso\n");
+	system("pause");
+}
+
